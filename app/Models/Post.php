61bb668215e24f9c514637ce54a0dbe5f1fae278
Fahri Anggara <fahriangga30@gmail.com>
2024-11-07 19:54:37 +0700

    Add description attribute to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -21,7 +22,7 @@ class Post extends Model
         'category_id',
     ];
 
-    protected $appends = ['thumbnail_url', 'create_at_diff'];
+    protected $appends = ['thumbnail_url', 'create_at_diff', 'description'];
 
     protected $with = ['category', 'user'];
 
@@ -65,5 +66,16 @@ class Post extends Model
     public function getCreateAtDiffAttribute()
     {
         return Carbon::parse($this->created_at)->locale('id')->diffForHumans();
+    }
+
+    /**
+     * getDescriptionAttribute
+     *
+     * @return void
+     */
+    public function getDescriptionAttribute()
+    {
+        // remove html tag and \n and limit 150
+        return Str::limit(strip_tags(str_replace("\n", '', $this->content)), 150);
     }
 }
