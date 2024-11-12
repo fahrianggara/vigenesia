4c6afdcbf6abe8f72579af720a80aee449e7778f
Fahri Anggara <fahriangga30@gmail.com>
2024-11-12 20:11:44 +0700

    Sort user posts by latest first

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,7 +72,7 @@ class User extends Authenticatable
      */
     public function posts()
     {
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Post::class)->latest();
     }
 
     /**
